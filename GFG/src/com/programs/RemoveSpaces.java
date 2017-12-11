2cbcfec758481395c2cc57798139b775fc9a990a
codelover224 <krushna.sharma12162643@gmail.com>
2017-12-12 01:00:27 +0530

    old code was not working so....new code

--- GFG/src/com/programs/RemoveSpaces.java
+++ GFG/src/com/programs/RemoveSpaces.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class RemoveSpaces {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		while (T-- > 0) {
-			String str;
-			str = sc.nextLine();
-			str = str.replace(" ", "");
-			System.out.println(str);
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		in.nextLine();
+		for (int i = 0; i < t; i++) {
+			String s[] = in.nextLine().split(" ");
+			for (int j = 0; j < s.length; j++)
+				System.out.print(s[j]);
+			System.out.println();
 		}
 	}
 }
