a86117118cb4bb97794b16119025095025811f1c
codelover224 <krushna.sharma12162643@gmail.com>
2017-12-15 13:12:18 +0530

    problem check

--- GFG/src/com/programs/ShortestUnorderedSubarray.java
+++ GFG/src/com/programs/ShortestUnorderedSubarray.java
@@ -23,8 +23,7 @@ public class ShortestUnorderedSubarray {
 	private static int check(int[] a) {
 		int p = 2;
 		if (a.length < 3)
-			return 0;
-		
+			return 0;	
 		else {
 			for (int i = 1; i < a.length - 1; i++) {
 				if (a[i - 1] < a[i] && a[i + 1] < a[i]) {
