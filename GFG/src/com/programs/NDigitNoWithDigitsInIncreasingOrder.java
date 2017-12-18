12f2dea1e764a2fcf1a7b736efe441ac912e5413
codelover224 <krushna.sharma12162643@gmail.com>
2017-12-18 12:01:33 +0530

    solved time limit exceeded problem

--- GFG/src/com/programs/NDigitNoWithDigitsInIncreasingOrder.java
+++ GFG/src/com/programs/NDigitNoWithDigitsInIncreasingOrder.java
@@ -1,29 +1,48 @@
 package com.programs;
+
 /*Given N, print all N digit numbers in increasing order, such that their digits are in strictly increasing order(from left to right).*/
+
 import java.util.Scanner;
 
 public class NDigitNoWithDigitsInIncreasingOrder {
-
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		while (t-- > 0) {
 			int n = sc.nextInt();
-			long num1 = (long) Math.pow(10, n - 1);
-			long num2 = (long) Math.pow(10, n);
+			long start = (long) Math.pow(10, n - 1);
+			long end = (long) Math.pow(10, n);
 
-			for (long i = num1; i < num2; i++)
-				if (check(i, n) != 0)
-					System.out.print(check(i, n) + " ");
+			for (long current = start; current < end; current++) {
+				if (check(current, n)) {
+					System.out.print(current + " ");
+				}
+			}
 			System.out.println();
 		}
 	}
-	private static long check(long i, int n) {
-		long res = 0;
-		for (int j = 0; j < n; j++)
-			if (i % 10 > check(i / 10, n - 1))
-				res = i;
-		return res;
+
+	private static boolean check(long current, int limit) {
+
+		long unitPlace = current % 10;
+		current = current / 10;
+		boolean status = true;
+		for (int i = 0; i < limit; i++) {
+			long upcommingDigit = current % 10;
+			// System.err.println("upcommingDigit : "+nextDigit);
+			// System.err.println("unitPlace : "+unitPlace);
+			if (upcommingDigit < unitPlace) {
+
+			} else {
+				status = false;
+			}
+
+			unitPlace = upcommingDigit;
+			current = current / 10;
+		}
+
+		return status;
 	}
 
 }
