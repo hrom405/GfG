e7a466cd772b20f8bd38b04d6755d5c21e836122
codelover224 <krushna.sharma12162643@gmail.com>
2017-12-20 02:42:58 +0530

    name

--- GFG/src/com/programs/FindWinnerOfGameOfNim.java
+++ GFG/src/com/programs/FindWinnerOfGameOfNim.java
@@ -1,8 +1,8 @@
 package com.programs;
 /*In Game of Nim, two players take turns removing objects from heaps or the pile of stones.
-Suppose two players A and B are playing the game. Each is allowed to take only one stone from the pile. 
-The player who picks the last stone of the pile will win the game. 
-Given N the number of stones in the pile, the task is to find the winner, if player A starts the game .*/
+ * Suppose two players A and B are playing the game. Each is allowed to take only one stone from the pile. 
+ * The player who picks the last stone of the pile will win the game. 
+ * Given N the number of stones in the pile, the task is to find the winner, if player A starts the game .*/
 
 import java.util.Scanner;
 
