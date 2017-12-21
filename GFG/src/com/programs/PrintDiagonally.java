796bcdb7f40ee1311cf9b403dd8478c730f11935
codelover224 <krushna.sharma12162643@gmail.com>
2017-12-22 02:11:40 +0530

    finally correct code

--- GFG/src/com/programs/PrintDiagonally.java
+++ GFG/src/com/programs/PrintDiagonally.java
@@ -1,4 +1,5 @@
 package com.programs;
+/*https://practice.geeksforgeeks.org/problems/print-diagonally/0*/
 
 import java.util.Scanner;
 
@@ -9,31 +10,34 @@ public class PrintDiagonally {
 		int t = sc.nextInt();
 		while (t-- > 0) {
 			int n = sc.nextInt();
-			int[][] a = new int[n][n];
-			for (int i = 0; i < n; i++)
-				for (int j = 0; j < n; j++)
-					a[i][j] = sc.nextInt();
-			for (int i = 0; i < n; i++)
-				for (int j = 0; j < n; j++)
-					System.out.print(a[i][j] + " ");
-			// print(a, n);
-		}
-	}
+			int[][] matrix = new int[n][n];
+			// int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
-	private static void print(int[][] a, int n) {
-		// int row = n;
-		// int col = n;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (i == j) {
-					System.out.print(a[i][j] + " ");
-				} else {
-					while (i++ != j--) {
-						System.out.print(a[i][j]);
-					}
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < n; j++)
+					matrix[i][j] = sc.nextInt();
+
+			int row = 0;
+			int col = 0;
+			for (int i = 0; i < n * 2 - 1; i++) {
+				if (col < n - 1) {
+					print(matrix, row, col, n);
+					col++;
+				}
+				if (col == n - 1) {
+					print(matrix, row, col, n);
+					row++;
 				}
 			}
+			System.out.println();
 		}
 	}
 
+	public static void print(int[][] matrix, int row, int col, int n) {
+		while (row < n && col >= 0) {
+			System.out.print(matrix[row][col] + " ");
+			row++;
+			col--;
+		}
+	}
 }
