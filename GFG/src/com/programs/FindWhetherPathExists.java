0a7a6841a1fbb9cab9352a1914cf7f9164ec6428
codelover224 <krushna.sharma12162643@gmail.com>
2017-12-24 02:45:04 +0530

    final correct/working code

--- GFG/src/com/programs/FindWhetherPathExists.java
+++ GFG/src/com/programs/FindWhetherPathExists.java
@@ -1,73 +1,65 @@
 package com.programs;
-
-import java.util.Arrays;
+/*https://practice.geeksforgeeks.org/problems/find-whether-path-exist/0/?ref=self*/
 import java.util.Scanner;
 
 public class FindWhetherPathExists {
-	static int[][] matrix = {{3,1,2},
-			  				 {3,0,0},
-			  				 {3,3,2}};
+	// static int[][] matrix = { { 0, 3, 2 }, { 3, 3, 0 }, { 1, 3, 0 } };
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//int t = sc.nextInt();
-		//while (t-- > 0) {
-			//int n = sc.nextInt();
-			
-			/*for (int i = 0; i < n; i++)
-				for (int j = 0; j < n; j++)
-					matrix[i][j] = sc.nextInt();*/
-
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
-					if (matrix[i][j] == 1) {
-						System.err.println("i==>" + i + "\t\tj==>" + j + "\n");
-						System.out.println(check(matrix, i, j));
-					}
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int[][] matrix = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = sc.nextInt();
 				}
 			}
+			boolean found = false;
+			boolean visited[][] = new boolean[n][n];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (matrix[i][j] == 1) {
+						// System.err.println("i==>" + i + "\t\tj==>" + j + "\n");
+						found = check(matrix, i, j, visited);
+						if (found)
+							break;
+					}
+				}
+				if (found)
+					break;
+			}
+			if (found)
+				System.out.println(1);
+			else
+				System.out.println(0);
 		}
-	//}
-
-	private static String check(int[][] matrix, int i, int j) {
-		print(matrix);
-		String ans = "No";
-		try {
-			if (matrix[i - 1][j] == 2 || matrix[i][j + 1] == 2 || matrix[i + 1][j] == 2 || matrix[i][j - 1] == 2) {
-				return "Yes";
-			}
-		} catch (ArrayIndexOutOfBoundsException e) {
-		}
-		matrix[i][j] = 0;
-		try {
-			if (matrix[i][j - 1] == 3) {
-				System.out.println("i==>" + i + "\tj==>" + (j - 1));
-				check(matrix, i, j - 1);
-			}
-			if (matrix[i - 1][j] == 3) {
-				System.out.println("i==>" + i + "\tj==>" + j);
-				check(matrix, i - 1, j);
-			}
-			if (matrix[i][j + 1] == 3) {
-				System.out.println("i==>" + i + "\tj==>" + j);
-				check(matrix, i, j + 1);
-			}
-			if (matrix[i + 1][j] == 3) {
-				System.out.println("i==>" + i + "\tj==>" + j);
-				check(matrix, i + 1, j);
-			}
-
-		} catch (ArrayIndexOutOfBoundsException e) {
-			//e.printStackTrace();
-		}
-		return ans;
 	}
-	public static void print(int[][] matrix) {
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				System.out.print(matrix[i][j] + " ");
-			}
-			System.out.println();
-			}
+
+	private static boolean check(int[][] matrix, int i, int j, boolean visited[][]) {
+		// System.out.println("i==>" + i + "\tj==>" + j);
+
+		if (j < 0 || j >= matrix[0].length || i < 0 || i >= matrix.length || visited[i][j])
+			return false;
+
+		if (matrix[i][j] == 2)
+			return true;
+
+		if (matrix[i][j] == 0)
+			return false;
+
+		if (matrix[i][j] == 1) {
+			visited[i][j] = true;
+			return check(matrix, i - 1, j, visited) || check(matrix, i + 1, j, visited)
+					|| check(matrix, i, j - 1, visited) || check(matrix, i, j + 1, visited);
+		}
+
+		if (matrix[i][j] == 3) {
+			visited[i][j] = true;
+			return check(matrix, i - 1, j, visited) || check(matrix, i + 1, j, visited)
+					|| check(matrix, i, j - 1, visited) || check(matrix, i, j + 1, visited);
+		} else
+			return false;
 	}
 }
