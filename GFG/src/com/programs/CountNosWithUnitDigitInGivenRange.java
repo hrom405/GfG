8eb219f6c6eb695cbe2a71ff00717f71d6a86856
codelover224 <krushna.sharma12162643@gmail.com>
2017-12-25 17:43:43 +0530

    new code

--- GFG/src/com/programs/CountNosWithUnitDigitInGivenRange.java
+++ GFG/src/com/programs/CountNosWithUnitDigitInGivenRange.java
@@ -3,7 +3,7 @@ package com.programs;
 import java.util.Scanner;
 
 public class CountNosWithUnitDigitInGivenRange {
-
+	static double startTime;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
@@ -11,20 +11,36 @@ public class CountNosWithUnitDigitInGivenRange {
 			int low = sc.nextInt();
 			int high = sc.nextInt();
 			int k = sc.nextInt();
-
-			System.out.println(check(low, high, k));
-
+			startTime=System.currentTimeMillis();
+			check(low, high, k);
+			
 		}
 	}
 
-	private static int check(int low, int high, int k) {
+	private static void check(int low, int high, int k) {
 		int cnt = 0;
-		for (int i = low; i <= high; i++) {
+		int n =0;
+		int llow=low;
+		/*for (int i = low; i <= high; i++) {
 			if (i % 10 == k) {
+				System.out.println(i);
 				cnt++;
 			}
+		}*/
+		if(k<low) {
+			n=llow;
+			while(llow%10!=k) {
+				llow++;
+			}
+			k=llow;
 		}
-		return cnt;
+		//System.out.println("llow==>" + llow);
+		while(k<=high && k>=low) {
+			//System.out.println(k);
+			k+=10;
+			cnt++;
+		}
+		System.out.println(cnt);
 	}
 
 }
