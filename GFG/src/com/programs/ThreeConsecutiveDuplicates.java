97f6c4b4464cdb8b2c4d5bfc85f47d8b8afb5224
codelover224 <krushna.sharma12162643@gmail.com>
2018-01-10 01:59:08 +0530

    new code

---
+++ GFG/src/com/programs/ThreeConsecutiveDuplicates.java
@@ -0,0 +1,45 @@
+package com.programs;
+
+/*https://practice.geeksforgeeks.org/problems/three-consecutive-duplicates/0/?ref=self*/
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ThreeConsecutiveDuplicates {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			String s = sc.next();
+			System.out.println(check(s));
+		}
+	}
+
+	private static String check(String s) {
+		int flag = 0;
+		ArrayList<Character> al = new ArrayList<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			al.add(s.charAt(i));
+		}
+		while (flag != 1) {
+			flag = 1;
+			for (int i = 0; i < al.size() - 2; i++) {
+				if (al.get(i) == al.get(i + 1) && al.get(i + 1) == al.get(i + 2)) {
+					al.remove(i);
+					al.remove(i);
+					al.remove(i);
+					flag = 0;
+				}
+			}
+		}
+		String str = "";
+		for (int i = 0; i < al.size(); i++) {
+			str = str + al.get(i);
+		}
+		if (str.equals(""))
+			return "-1";
+		else
+			return str;
+	}
+}
