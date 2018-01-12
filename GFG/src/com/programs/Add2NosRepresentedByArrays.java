a8bdcef434bcca00ae7f2532d6c58ab4b602c5bf
codelover224 <krushna.sharma12162643@gmail.com>
2018-01-13 01:04:24 +0530

    new codes

---
+++ GFG/src/com/programs/Add2NosRepresentedByArrays.java
@@ -0,0 +1,35 @@
+package com.programs;
+
+/*https://practice.geeksforgeeks.org/problems/add-two-numbers-represented-by-two-arrays/0/?ref=self*/
+import java.util.Scanner;
+
+public class Add2NosRepresentedByArrays {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for (int i = 0; i < a.length; i++) {
+				a[i] = sc.nextInt();
+			}
+			int m = sc.nextInt();
+			int[] b = new int[m];
+			for (int i = 0; i < b.length; i++) {
+				b[i] = sc.nextInt();
+			}
+			StringBuilder ab = new StringBuilder();
+			StringBuilder bb = new StringBuilder();
+			for (int i = 0; i < a.length; i++) {
+				ab.append(a[i] + "");
+			}
+			for (int i = 0; i < b.length; i++) {
+				bb.append(b[i] + "");
+			}
+			System.out.println(Integer.valueOf(ab.toString()) + Integer.valueOf(bb.toString()));
+		}
+
+	}
+
+}
