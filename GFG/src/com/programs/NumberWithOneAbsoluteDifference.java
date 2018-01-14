564c1dc5fec3c87a129c4fc663843c950ebbd30a
codelover224 <krushna.sharma12162643@gmail.com>
2018-01-14 23:58:19 +0530

    incomplete code

---
+++ GFG/src/com/programs/NumberWithOneAbsoluteDifference.java
@@ -0,0 +1,16 @@
+package com.programs;
+
+import java.util.Scanner;
+
+public class NumberWithOneAbsoluteDifference {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int[] a  = new int[n];
+		}
+	}
+
+}
