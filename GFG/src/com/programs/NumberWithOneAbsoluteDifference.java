e66c719b65fabbb5c658cfe6ed0bdbde0c3e2ecd
codelover224 <krushna.sharma12162643@gmail.com>
2018-01-15 12:11:28 +0530

    final complete code

--- GFG/src/com/programs/NumberWithOneAbsoluteDifference.java
+++ GFG/src/com/programs/NumberWithOneAbsoluteDifference.java
@@ -1,5 +1,7 @@
 package com.programs;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class NumberWithOneAbsoluteDifference {
@@ -8,9 +10,40 @@ public class NumberWithOneAbsoluteDifference {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		while (t-- > 0) {
-			int n = sc.nextInt();
-			int[] a  = new int[n];
+			long n = sc.nextLong();
+			if (n <= 9)
+				System.out.println(-1);
+			else {
+				Queue<Long> q = new LinkedList<>();
+
+				for (long i = 1; i <= 9; i++) {
+					q.add(i);
+				}
+				long temp = q.peek();
+				while (temp < n) {
+					temp = q.poll();
+					if (temp >= 10 && temp <= n) {
+						System.out.print(temp + " ");
+					}
+					long last = temp % 10;
+					long number = temp;
+					long add;
+					if (last == 0) {
+						add = number * 10 + 1;
+						q.add(add);
+
+					} else if (last == 9) {
+						add = number * 10 + 8;
+						q.add(add);
+					} else {
+						add = number * 10 + last - 1;
+						q.add(add);
+						add = number * 10 + last + 1;
+						q.add(add);
+					}
+				}
+				System.out.println();
+			}
 		}
 	}
-
 }
