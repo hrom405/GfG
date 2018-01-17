cf14d4fcb5e77fc57686fa21a12b9d30683e5ffb
codelover224 <krushna.sharma12162643@gmail.com>
2018-01-17 19:06:44 +0530

    new codes

---
+++ GFG/src/com/programs/IsogramString.java
@@ -0,0 +1,24 @@
+package com.programs;
+
+/*https://practice.geeksforgeeks.org/problems/check-if-a-string-is-isogram-or-not/0/?ref=self*/
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class IsogramString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			String s = sc.next();
+			HashMap<Character, Integer> h = new HashMap<>();
+			for (int i = 0; i < s.length(); i++) {
+				h.put(s.charAt(i), 1);
+			}
+			if (h.size() == s.length())
+				System.out.println(1);
+			else
+				System.out.println(0);
+		}
+	}
+}
---
+++ GFG/src/com/programs/PowerOfOddNumbers.java
@@ -0,0 +1,24 @@
+package com.programs;
+
+/*https://practice.geeksforgeeks.org/problems/power-of-pow-odd-numbers/0/?ref=self*/
+import java.util.Scanner;
+
+public class PowerOfOddNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			long sum = 0;
+
+			for (int i = 1; i <= n; i++) {
+				int z = (i - 1) * 2 + 1;
+				sum += Math.pow(z, 2.0);
+			}
+
+			System.out.println(sum);
+
+		}
+	}
+}
