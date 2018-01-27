f87613d550a5ad093ec769b77dad1ada3fff2e68
codelover224 <krushna.sharma12162643@gmail.com>
2018-01-27 23:56:19 +0530

    incomplete code

---
+++ GFG/src/com/programs/NumberWithPrimeFreqGreaterOnEqualToK.java
@@ -0,0 +1,26 @@
+package com.programs;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class NumberWithPrimeFreqGreaterOnEqualToK {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int  t= sc.nextInt();
+		while(t-->0) {
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for (int i = 0; i < a.length; i++) {
+				a[i]=sc.nextInt();
+			}
+			Map<Integer, Integer> hm = new HashMap<>();
+			for (int i = 0; i < a.length; i++) {
+				
+			}
+		}
+
+	}
+
+}
