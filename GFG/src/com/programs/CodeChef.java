677cf07b8613b2bf498efc466363773ffa105020
codelover224 <krushna.sharma12162643@gmail.com>
2018-02-12 23:52:23 +0530

    new code

---
+++ GFG/src/com/programs/CodeChef.java
@@ -0,0 +1,31 @@
+package com.programs;
+
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+	public static void main(String[] args) throws java.lang.Exception {
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while (t-- > 0) {
+			int n = s.nextInt();
+			long a[] = new long[n];
+			for (int j = 0; j < n; j++) {
+				a[j] = s.nextLong();
+			}
+
+			long c = s.nextLong();
+			long d = s.nextLong();
+			long ss = s.nextLong();
+
+			double m = a[0];
+			for (int i = 0; i < n - 1; i++) {
+				if (a[i + 1] > a[i]) {
+					m += a[i + 1] - a[i];
+				}
+			}
+			System.out.printf("%.9f", m * (c - 1));
+
+		}
+	}
+}
