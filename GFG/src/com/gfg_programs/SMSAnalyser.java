6afa3a0e70407639e920e9ae36a60529af8d3eac
codelover224 <krushna.sharma12162643@gmail.com>
2018-03-28 23:28:32 +0530

    new code

---
+++ GFG/src/com/gfg_programs/SMSAnalyser.java
@@ -0,0 +1,32 @@
+package com.gfg_programs;
+
+import java.util.Scanner;
+
+public class SMSAnalyzer {
+	static String[] str = { "love", "babe", "sweety", "life" };
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String[] words = s.split("\\s");
+		// System.out.println(Arrays.toString(words));
+		int a = 0;
+		int len = words.length;
+		for (int i = 0; i < words.length; i++) {
+			for (int j = 0; j < str.length; j++) {
+				if (words[i].equals(str[j])) {
+					a++;
+				}
+			}
+		}
+		int div = 2;
+		while (div <= words.length) {
+			if (a % div == 0 && len % div == 0) {
+				a /= div;
+				len /= div;
+			} else
+				div++;
+		}
+		System.out.println(a + "/" + len);
+	}
+}
